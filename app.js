65a60a2d7195ba84dab0772f3199ccdc8d5bb591
Maks Nikulnikov <5286433@gmail.com>
2023-05-11 15:52:20 +0300

    feat: add helmet

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cors = require('cors');
+const helmet = require('helmet');
 
+const router = require('./routes/index');
+const errorHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(cors());
-mongoose.connect(process.env.DB_ADRESS, {
-  autoIndex: true,
-});
 
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
-
-app.use('/', require('./routes/index'));
-
+app.use('/', router);
 app.use(errorLogger);
 app.use(errors());
-app.use(require('./middlewares/errors'));
+app.use(errorHandler);
 
+mongoose.connect(process.env.DB_ADRESS, {
+  autoIndex: true,
+});
 app.listen(PORT, () => {
 });
