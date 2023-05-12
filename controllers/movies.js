eacf2f6eb2db1459692bec32bc84901dce0839ca
Maks Nikulnikov <5286433@gmail.com>
2023-05-12 15:30:28 +0300

    fix: get/movies returns current user's movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -12,7 +12,7 @@ const {
 } = require('../constants');
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
+  Movie.find({ owner: req.user._id })
     .then((card) => res.send(card))
     .catch(next);
 };
